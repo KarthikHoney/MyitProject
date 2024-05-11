9fc83a732fc83cc85d36d9186f0c8edd73dfd5af
KarthikHoney <jacksparowkarthick@gmail.com>
2024-05-11 14:08:44 +0530

    dot

--- login.php
+++ login.php
@@ -4,13 +4,14 @@ include "connection.php";
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-$sql = "SELECT * FROM login WHERE `username`='admin' AND `password`='password'";
+$sql ="SELECT username password FROM login WHERE username='admin' AND password='password'";
 $result = mysqli_query($conn, $sql);
 
 if ($result) {
     // Redirect to the home page
     header("Location: home.html");
-    exit(); // Make sure no further PHP code gets executed after the redirect
+    echo 'data inserted successfully';
+    // Make sure no further PHP code gets executed after the redirect
 } else {
     echo "Username or password incorrect";
 }
